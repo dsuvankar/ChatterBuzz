e4d44f0cd30caa70301f5ed613c61e27b87958ba
Suvankar Das <iamsuvankar25@gmail.com>
2024-11-26 13:54:21 +0530

    backend done

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+
 import { connectDB } from "./lib/db.js";
 
 const app = express();
@@ -12,6 +14,7 @@ const PORT = process.env.PORT;
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 app.listen(PORT, () => {
   console.log(`server is up and running at ${PORT}`);
