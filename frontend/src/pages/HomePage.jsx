50c50efe914b9dc615aa9c7885a9dce7b4e3e4e9
Suvankar Das <iamsuvankar25@gmail.com>
2024-11-29 14:06:45 +0530

    message container updated

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -7,10 +7,12 @@ import ChatContainer from "../components/ChatContainer";
 const HomePage = () => {
   const { selectedUser } = useChatStore();
   return (
-    <div className="h-screen bg-base-200">
-      <div className="flex items-center justify-center pt-20 px-4">
-        <div className="bg-base-100 rounded-lg shadow-cl w-full max-w-6xl h-[calc(100vh-8rem)]">
-          <div className="flex h-full rounded-lg overflow-hidden">
+    <div className="h-full overflow-x-hidden  bg-base-200">
+      <div className="flex items-center justify-center pt-20 p-2">
+        <div
+          className="bg-base-100 rounded-lg shadow-cl w-full max-w-6xl h-[calc(100vh-5rem)]
+">
+          <div className="flex h-full rounded-lg ">
             <Sidebar />
 
             {!selectedUser ? <NoChatSelected /> : <ChatContainer />}
